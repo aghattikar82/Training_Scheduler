33fb56aef1f4190f20b5dd0606985531c95ccf0b
Anup Ghattikar <aghattikar82@gmail.com>
2026-03-23 21:01:02 +0530

    Update coursename.js

--- src/components/coursename.js
+++ src/components/coursename.js
@@ -176,6 +176,5 @@ export const courseList = [
     "AI-Empowered SAFe® 6.0 Scrum Master (SSM) Certification Training",
     "Professional Scrum Master–AI Essentials (PSM-AI) Certification Training",
     "Professional Scrum Product Owner–AI Essentials (PSPO-AI Essentials) Certification Training",
-    "PMI-CPMAI Certification Training",
     "Others"
   ];
